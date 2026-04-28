74a2ac395ac7d1903529dd231def982a93a9d690
Minindu <mininduthiranjaya50@gmail.com>
2026-04-28 12:03:18 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const isGitHubPages = process.env.DEPLOY_TARGET === "gh-pages";
-
-  return {
-    base: isGitHubPages ? "/forkify-vite/" : "/",
-  };
+export default defineConfig({
+  base: "/",
 });
