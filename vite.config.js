37c70e54e3a065dd6e29700186512c50e2e32b26
Minindu <mininduthiranjaya50@gmail.com>
2026-04-28 11:19:27 +0530

    add env file and update config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(() => {
-  // Read custom environment variable
+export default defineConfig(({ mode }) => {
   const isGitHubPages = process.env.DEPLOY_TARGET === "gh-pages";
 
   return {
