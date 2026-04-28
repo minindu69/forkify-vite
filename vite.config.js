a06d0d35e404676e8577fe596069508a3bd86b3a
Minindu <mininduthiranjaya50@gmail.com>
2026-04-28 10:47:16 +0530

    updated congig file and package.json file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/forkify-vite/",
+export default defineConfig(() => {
+  // Read custom environment variable
+  const isGitHubPages = process.env.DEPLOY_TARGET === "gh-pages";
+
+  return {
+    base: isGitHubPages ? "/forkify-vite/" : "/",
+  };
 });
